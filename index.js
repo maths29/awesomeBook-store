c5e3ff63369dbd01fe2c5087805d516a6f82a15e
mathsoft29 <iwuconcept@gmail.com>
2023-06-07 08:55:34 +0100

    refactored js file and update the style.css

--- index.js
+++ index.js
@@ -1,57 +1,74 @@
+/* eslint-disable no-unused-vars */
 const BooksContainer = document.querySelector('.books-list');
 const bookTitle = document.querySelector('.bookTitle');
 const bookAuthor = document.querySelector('.bookAutor');
 const form = document.querySelector('form');
-
-let BooksList = [];
-
-const booksInStore = localStorage.getItem('BooksList');
-if (booksInStore) {
-  BooksList = JSON.parse(booksInStore);
-}
-
+let Books = [];
+const booksInStore = localStorage.getItem('Books');
 function createBooksLst() {
   BooksContainer.innerHTML = '';
-  BooksList.forEach((book, index) => {
+  Books.forEach((book, index) => {
     const bookDiv = document.createElement('div');
+
     bookDiv.innerHTML = `
         <ul>
-            <li>${book.title}</li>
-            <li>${book.author}</li>
-            <button onclick="removeBook(${index})">Remove</button>
+            <li>"${book.title}" by ${book.author}</li>
+            <button class="removeB" onClick="removeB(${index})">Remove</button>
         </ul>
-        <hr>
+        <br>
     `;
     BooksContainer.appendChild(bookDiv);
   });
 }
 
-/* ------------Add New Book to The List-------------- */
-function addBook(e) {
+class Bookslist {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+
+  addBook() {
+    const newBooks = {
+      title: bookTitle.value,
+      author: bookAuthor.value,
+    };
+
+    Books.push(newBooks);
+    localStorage.setItem('books', JSON.stringify(Books));
+
+    createBooksLst();
+
+    // Reset input fields
+    bookTitle.value = '';
+    bookAuthor.value = '';
+    return this;
+  }
+
+  removeBook(index) {
+    Books = Books.filter((book, bookIndex) => bookIndex !== index);
+    localStorage.setItem('BooksList', JSON.stringify(Books));
+
+    createBooksLst();
+    return this;
+  }
+}
+
+const newBook = new Bookslist(bookTitle.value, bookAuthor.value);
+
+/* ------------Add the Book to The List-------------- */
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const newBooks = {
-    title: bookTitle.value,
-    author: bookAuthor.value,
-  };
+  newBook.addBook();
+});
+/* ------------Add the Book to The List-------------- */
 
-  BooksList.push(newBooks);
-  localStorage.setItem('books', JSON.stringify(BooksList));
-
-  createBooksLst();
-
-  // Reset input fields
-  bookTitle.value = '';
-  bookAuthor.value = '';
+/* ------------Remove the Book from The List-------------- */
+function removeB(index) {
+  newBook.removeBook(index);
 }
-/* ------------Add New Book to The List-------------- */
-
-function removeBook(index) {
-  BooksList = BooksList.filter((book, bookIndex) => bookIndex !== index);
-  localStorage.setItem('BooksList', JSON.stringify(BooksList));
-
-  createBooksLst();
+/* ------------Remove the Book from The List-------------- */
+if (booksInStore) {
+  Books = JSON.parse(booksInStore);
 }
 
-form.addEventListener('submit', addBook);
-removeBook();
 createBooksLst();
